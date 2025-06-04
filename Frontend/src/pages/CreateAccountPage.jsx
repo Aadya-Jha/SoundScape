a28bb5febaf20bdb616ab50662e5f2ed16a53fbb
aadya-jha <aadya.aarchi@gmail.com>
2025-06-04 20:21:11 +0530

    configured tailwind

--- Frontend/src/pages/CreateAccountPage.jsx
+++ Frontend/src/pages/CreateAccountPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CreateAccountPage = () => {
   return (
     <div>
-      <h2>Create Account</h2>
+      <h2 className='text-2xl font-extrabold'>Create Account</h2>
     </div>
   );
 };

fd6012ac298c5b13c557e28506e7ed353b36c9ba
aadya-jha <aadya.aarchi@gmail.com>
2025-06-06 22:38:22 +0530

    user profile integrated

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,8 +1,13 @@
 const getUserProfile = (req, res) => {
-    const user = {
-        name: "John Doe",
-        gmail: "john.doe@gmail.com"
-    };
+    const user = [
+        {
+            username: "john123",
+            name: "John Doe",
+            email: "john@example.com",
+            bio: "Music lover and developer",
+            avatarURL: "https://example.com/john-avatar.jpg",
+        }
+    ];
     res.json(user);
 };
 

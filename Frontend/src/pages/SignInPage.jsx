beb1add471c2ce039e36ba5d1a97ecf2993f2e93
Apoorva Santhosh <apoorvas2040@gmail.com>
2025-06-06 19:08:44 +0530

    sign-in page

--- Frontend/src/pages/SignInPage.jsx
+++ Frontend/src/pages/SignInPage.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignInPage.css';
 
 const SignInPage = () => {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <div className="page-container">
+        <div className="page-container">
       <h2>Sign In</h2>
+      <div className="form-box">
+        <form>
+          <div>
+            <label htmlFor="username">Username</label>
+            <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)} />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+            type="text"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <button type="submit">Sign In</button>
+        </form>
+      </div>
     </div>
   );
 };

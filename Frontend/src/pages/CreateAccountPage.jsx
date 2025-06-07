40c2892da186a185848110f71ed2054a7668515f
aadya-jha <aadya.aarchi@gmail.com>
2025-06-07 17:03:51 +0530

    styling changes

--- Frontend/src/pages/CreateAccountPage.jsx
+++ Frontend/src/pages/CreateAccountPage.jsx
@@ -2,11 +2,10 @@ import React, { useState } from 'react';
 import './CreateAccountPage.css';
 
 const CreateAccountPage = () => {
-
-const [username, setUsername] = useState('');
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
-const [confirmPassword, setConfirmPassword] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   return (
     <div className="page-container">
@@ -16,36 +15,40 @@ const [confirmPassword, setConfirmPassword] = useState('');
           <div>
             <label htmlFor="username">Username</label>
             <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)} />
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
           <div>
             <label htmlFor="email">Email</label>
             <input
-            type="text"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)} />
+              type="text"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div>
             <label htmlFor="password">Password</label>
             <input
-            type="text"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)} />
+              type="text"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <div>
             <label htmlFor="confirmPassword">Confirm Password</label>
             <input
-            type="text"
-            id="confirmPassword"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)} />
-           </div>
-           <button type="submit">Create Account</button>
+              type="text"
+              id="confirmPassword"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit">Create Account</button>
         </form>
       </div>
     </div>

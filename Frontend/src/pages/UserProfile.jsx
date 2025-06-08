818a23880bdb187a71978e18593fff6131331eaa
Apoorva Santhosh <apoorvas2040@gmail.com>
2025-06-08 09:10:05 +0530

    minor changes in UserProfile

--- Frontend/src/pages/UserProfile.jsx
+++ Frontend/src/pages/UserProfile.jsx
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from "react";
+import "./UserProfile.css";
 
 const UserProfile = () => {
     const [profile, setProfile] = useState({});
@@ -14,15 +15,23 @@ const UserProfile = () => {
     fetchProfile();
     }, []);
 
-    return(
-        <div>
-            <h1>{profile.username }</h1>
-            <p>{profile.name}</p>
-            <p>{profile.email}</p>
-            <p>{profile.bio}</p>
-            <img src={profile.avatarURL} alt="" />
+    return (
+    <div className="profile-container">
+        <div className="profile-content">
+             <img className="profile-avatar" src={profile.avatarURL} alt="Profile Avatar" />
+             <div className="profile-info">
+               <h1>{profile.username}</h1>
+               <p>{profile.name}</p>
+               <p>{profile.email}</p>
+               <p>{profile.bio}</p>
+             </div>
         </div>
-    );
+        <div className="user-activity">
+            <h2>Your Reviews</h2>
+        </div>
+    </div>
+);
+
 
 }
 

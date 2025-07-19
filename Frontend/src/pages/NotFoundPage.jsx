314d57e2ae38a0c527bbdab0506b92f890360f76
Apoorva Santhosh <apoorvas2040@gmail.com>
2025-07-19 18:43:21 +0530

    NotFoundPage done

---
+++ Frontend/src/pages/NotFoundPage.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './NotFound.css'; 
+
+const NotFound = () => {
+  return (
+    <div className="page-container">
+      <h1>404</h1>
+      <h2>Oops! Page not found.</h2>
+      <p>
+        The page you're looking for doesn't exist or has been moved. Let’s help
+        you get back on track.
+      </p>
+      <Link to="/" className="back-button">
+        Back to Home
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;

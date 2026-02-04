6fed4fb0d5b755dcdee6e9ca15bdbee53a378fa6
aadya-jha <aadya.aarchi@gmail.com>
2026-02-04 11:28:55 +0530

    api integration

--- Backend/src/controllers/musicController.js
+++ Backend/src/controllers/musicController.js
@@ -1,15 +1,11 @@
-import fetch from "node-fetch";
-
 export const searchArtists = async (req, res) => {
   try {
-    // STEP A: read query
     const query = req.query.q;
 
     if (!query) {
       return res.status(400).json({ message: "Missing search query" });
     }
 
-    // STEP B: call MusicBrainz artist search
     const response = await fetch(
       `https://musicbrainz.org/ws/2/artist/?query=${query}&fmt=json&limit=5`,
       {
@@ -18,17 +14,45 @@ export const searchArtists = async (req, res) => {
         },
       }
     );
+    const data = await response.json(); 
 
-    const data = await response.json();
-
-    // STEP C: clean the data
     const artists = data.artists.map((artist) => ({
       id: artist.id,
       name: artist.name,
     }));
 
-    // STEP D: return result
-    res.json({ artists });
+    const songResponse = await fetch(
+      `https://musicbrainz.org/ws/2/recording/?query=${query}&fmt=json&limit=5`,
+      {
+        headers: {
+          "User-Agent": "SoundScape/1.0 ( learning project )",
+        },
+      }
+    );
+    const songData = await songResponse.json();
+
+    const songs = songData.recordings.map((record) => ({
+      id: record.id,
+      title: record.title,
+      artist: record["artist-credit"]?.[0]?.name || "Unknown",
+    }));
+
+    const albumResponse = await fetch(
+      `https://musicbrainz.org/ws/2/release/?query=${query}&fmt=json&limit=5`,
+      {
+        headers: { 
+          "User-Agent": "SoundScape/1.0 ( learning project )", 
+        },
+      }
+    );
+    const albumData = await albumResponse.json();
+    const albums = albumData.releases.map((release) => ({
+      id: release.id,
+      title: release.title,
+      artist: release["artist-credit"]?.[0]?.name || "Unknown",
+    }));
+
+    res.json({ artists, songs, albums });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
